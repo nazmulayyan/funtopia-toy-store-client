cc0b531515489261cab197ffc6825175c62a5c25
NazmulAyyan <rnazmul2424@gmail.com>
2023-05-27 12:50:47 +0600

    aos animation added on home page

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import img1 from '../../../assets/gallrey/img1.png'
 import img2 from '../../../assets/gallrey/img2.png'
 import img3 from '../../../assets/gallrey/img3.png'
@@ -6,35 +7,55 @@ import img5 from '../../../assets/gallrey/img5.png'
 import img6 from '../../../assets/gallrey/img6.png'
 import img7 from '../../../assets/gallrey/img7.png'
 import img8 from '../../../assets/gallrey/img8.png'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Gallery = () => {
+
+    useEffect(()=>{
+        Aos.init({
+            duration: 2000,
+
+        })
+    },[])
+
     return (
         <div className='lg:max-w-screen-2xl lg:p-0 px-5 m-auto mb-36'>
             <h3 className='text-center font-bold text-4xl uppercase text-pink-500 for-border pb-20'>toys gallrey</h3>
             <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 '>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img1} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img2} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img3} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img4} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img5} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img6} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img7} alt="" />
                 </div>
-                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200'>
+
+                <div className='p-14 shadow-xl rounded-xl border hover:shadow-none duration-200' data-aos='fade-up'>
                     <img className='w-72 h-72 m-auto' src={img8} alt="" />
                 </div>
+
             </div>
         </div>
     );
--- src/Pages/Home/TrandingToys/TrandingToys.jsx
+++ src/Pages/Home/TrandingToys/TrandingToys.jsx
@@ -1,44 +1,63 @@
+import { useEffect } from 'react'
 import img1 from '../../../assets/tranding/img1.png'
 import img2 from '../../../assets/tranding/img2.png'
 import img3 from '../../../assets/tranding/img3.png'
 import img4 from '../../../assets/tranding/img4.png'
 import img5 from '../../../assets/tranding/img5.png'
 import img6 from '../../../assets/tranding/img6.png'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const TrandingToys = () => {
+
+    useEffect(()=>{
+        Aos.init({
+            duration: 2000,
+
+        })
+    },[])
+
     return (
         <div className='lg:max-w-screen-2xl py-10 mb-36 px-5'>
             <h3 className='text-center font-bold text-4xl uppercase text-pink-500 for-border'>tranding toys</h3>
 
             <div className=''>
                 <div className='grid md:grid-cols-3 grid-cols-1 pt-36 gap-12'>
-                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10'>
+
+                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10' data-aos='fade-up'>
                         <img className='w-80 h-80 -mt-28 m-auto' src={img1} alt="" />
                         <h5 className='text-xl font-bold pt-10'>Toy Name: Diddy Knong</h5>
                     </div>
-                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28'>
+
+                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28' data-aos='fade-up'>
                         <img className='w-80 h-80 -mt-28 m-auto' src={img2} alt="" />
                         <h5 className='text-xl font-bold pt-10'>Toy Name: Diddy Knong</h5>
                     </div>
-                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28'>
+
+                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28' data-aos='fade-up'>
                         <img className='w-80 h-80 -mt-28 m-auto' src={img3} alt="" />
                         <h5 className='text-xl font-bold pt-10'>Toy Name: Diddy Knong</h5>
                     </div>
+
                 </div>
 
                 <div className='grid md:grid-cols-3 grid-cols-1 pt-36 gap-12'>
-                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-5'>
+
+                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-5' data-aos='fade-up'>
                         <img className='w-70 h-80 -mt-28 m-auto' src={img4} alt="" />
                         <h5 className='text-xl font-bold pt-10'>Toy Name: Diddy Knong</h5>
                     </div>
-                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28'>
+
+                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28' data-aos='fade-up'>
                         <img className='w-70 h-80 -mt-28 m-auto' src={img5} alt="" />
                         <h5 className='text-xl font-bold pt-10'>Toy Name: Diddy Knong</h5>
                     </div>
-                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28'>
+
+                    <div className='border shadow-xl rounded-xl duration-200 hover:shadow-none text-center px-10 pt-10 pb-10 md:mt-0 mt-28' data-aos='fade-up'>
                         <img className='w-70 h-80 -mt-28 m-auto' src={img6} alt="" />
                         <h5 className='text-xl font-bold pt-10'>Toy Name: Diddy Knong</h5>
                     </div>
+
                 </div>
 
             </div>
